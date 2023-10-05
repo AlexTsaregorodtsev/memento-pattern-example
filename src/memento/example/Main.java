eb4b913cfb3a5b215f166c9679cc5ae2ff551f33
Aleksander Tsaregorodtsev <CaregorodcevAlex@yandex.ru>
2023-10-05 22:57:08 +0700

    Add fontSize field and removed memento-pattern-example.iml which is now ignored.

--- src/memento/example/Main.java
+++ src/memento/example/Main.java
@@ -4,6 +4,8 @@ package memento.example;
 
 public class Main {
 
+    private static final String FONT_SIZE_TEXT = "Font size = %d.%n";
+
     public static void main(String[] args) {
         TextEditor textEditor = new TextEditor();
         TextManager textManager = new TextManager(textEditor);
@@ -13,20 +15,25 @@ public class Main {
 
         textManager.hitSave();
 
+        textEditor.setFontSize(20);
         textEditor.write("Use Memento when you need to make a snapshot of your object before changing it.\n");
         textManager.hitSave();
 
         textEditor.write("Buy milk and eggs before coming home\n");
 
+        System.out.printf(FONT_SIZE_TEXT, textEditor.getFontSize());
         System.out.println(textEditor.getText());
 
         textManager.hitUndo();
+        System.out.printf(FONT_SIZE_TEXT, textEditor.getFontSize());
         System.out.println(textEditor.getText());
 
         textManager.hitUndo();
+        System.out.printf(FONT_SIZE_TEXT, textEditor.getFontSize());
         System.out.println(textEditor.getText());
 
         textManager.hitUndo();
+        System.out.printf(FONT_SIZE_TEXT, textEditor.getFontSize());
         System.out.println(textEditor.getText());
     }
 
--- src/memento/example/TextEditor.java
+++ src/memento/example/TextEditor.java
@@ -5,10 +5,15 @@ package memento.example;
  */
 public class TextEditor {
 
+    private static final String FONT_SIZE_EXCEPTION = "Expected font size from 1 to 100 inclusive. Received %d.";
+
+    private static final int DEFAULT_FONT_SIZE = 12;
     private StringBuilder currentText;
+    private int fontSize;
 
     public TextEditor() {
         this.currentText = new StringBuilder();
+        this.fontSize = DEFAULT_FONT_SIZE;
     }
 
     public void write(String text) {
@@ -19,12 +24,24 @@ public class TextEditor {
         return currentText.toString();
     }
 
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        if (fontSize <= 0 || fontSize >= 100) {
+            throw new IllegalArgumentException(String.format(FONT_SIZE_EXCEPTION, fontSize));
+        }
+        this.fontSize = fontSize;
+    }
+
     public TextEditorState prepareMemento() {
-        return new TextEditorState(currentText.toString());
+        return new TextEditorState(currentText.toString(), fontSize);
     }
 
     public void restore(TextEditorState memento) {
         currentText = new StringBuilder(memento.getText());
+        fontSize = memento.getFontSize();
     }
 
     /**
@@ -33,13 +50,19 @@ public class TextEditor {
     public class TextEditorState {
 
         private final String text;
+        private final int fontSize;
 
-        private TextEditorState(String text) {
+        private TextEditorState(String text, int fontSize) {
             this.text = text;
+            this.fontSize = fontSize;
         }
 
         private String getText() {
             return text;
+        }
+
+        private int getFontSize() {
+            return fontSize;
         }
     }
 }
